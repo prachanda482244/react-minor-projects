6385b2259ab1358e421c01e729427dce96828b24
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2024-01-28 12:14:30 +0545

    notes for project 2

--- react-minor-project/src/Projects/Project2/Project2TextUtility.jsx
+++ react-minor-project/src/Projects/Project2/Project2TextUtility.jsx
@@ -2,6 +2,11 @@ const Project2TextUtility = () => {
     return (
         <div>
             Text Utility with useState - onchange
+            text area for the input
+            buttons for upper case , lower case  , copy , remove extra spaces
+            display for summary
+            and total words and character
+            preview
         </div>
     )
 }
